7f6a8c66182230398de7622ab962ad7249704eb8
Justin Welenofsky <welenofsky@gmail.com>
2017-03-09 09:54:56 -0800

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -26,6 +26,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", login_path, count: 0
 		assert_select "a[href=?]", logout_path
 		assert_select "a[href=?]", user_path(@user)
+		# Simulate a user clicking logout in a second window.
 		delete logout_path
 		assert_not is_logged_in?
 		assert_redirected_to root_url
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require 'rails/test_help'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 
+# used for unit tests
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
@@ -12,5 +13,20 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+# Used for integration tests
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 end

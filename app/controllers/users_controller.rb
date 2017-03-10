1d1008a16ce704e04f05d0c4bc2b949711761491
Justin Welenofsky <welenofsky@gmail.com>
2017-03-10 15:36:43 -0800

    10.31

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,62 @@
 class UsersController < ApplicationController
-	def new
-		@user = User.new
-	end
 
-	def show
-		@user = User.find(params[:id])
-		#debugger
-	end
+    before_action :logged_in_user, only: [:edit, :update]
+    before_action :correct_user,   only: [:edit, :update]
 
-	def create
-		@user = User.new(user_params)    # Not the final implementation!
-		if @user.save
-			log_in @user
-			flash[:success] = "Welcome to the Sample App!"
-			redirect_to @user
-		else
-			render 'new'
-		end
-	end
+    def new
+        @user = User.new
+    end
 
-	def edit
-		@user = User.find(params[:id])
-	end
+    def show
+        @user = User.find(params[:id])
+        #debugger
+    end
 
-	def update
-		@user = User.find(params[:id])
-		if @user.update_attributes(user_params)
-			flash[:success] = "Profile updated"
-			redirect_to @user
-		else
-			render 'edit'
-		end
-	end
+    def create
+        @user = User.new(user_params)    # Not the final implementation!
+        if @user.save
+            log_in @user
+            flash[:success] = "Welcome to the Sample App!"
+            redirect_to @user
+        else
+            render 'new'
+        end
+    end
 
-	private
-		# Visual indent is a convention to help visually show which methods are private
-		def user_params
-			params.require(:user).permit(:name, :email, :password, :password_confirmation)
-		end
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(user_params)
+            flash[:success] = "Profile updated"
+            redirect_to @user
+        else
+            render 'edit'
+        end
+    end
+
+    private
+        # Visual indent is a convention to help visually show which methods are private
+        def user_params
+            params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        end
+
+        # Before filters
+
+        # Confirms a logged-in user.
+        def logged_in_user
+            unless logged_in?
+                store_location
+                flash[:danger] = "Please log in."
+                redirect_to login_url
+            end
+        end
+
+        # Confirms the correct user.
+        def correct_user
+            @user = User.find(params[:id])
+            redirect_to(root_url) unless current_user?(@user)
+        end
 end

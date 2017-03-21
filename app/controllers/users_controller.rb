8716e59618c37fb5ff2bbc6abf5e1bb83fcf393e
Justin Welenofsky <welenofsky@gmail.com>
2017-03-21 14:45:04 -0700

    11.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,20 +15,21 @@ class UsersController < ApplicationController
     end
 
     def index
-        @users = User.paginate(page: params[:page])
+        @users = User.where(activated: true).paginate(page: params[:page])
     end
 
     def show
         @user = User.find(params[:id])
+        redirect_to root_url and return unless @user.activated?
         #debugger #byebug
     end
 
     def create
-        @user = User.new(user_params)    # Not the final implementation!
+        @user = User.new(user_params)
         if @user.save
-            log_in @user
-            flash[:success] = "Welcome to the Sample App!"
-            redirect_to @user
+            @user.send_activation_email
+            flash[:info] = "Please check your email to activate your account."
+            redirect_to root_url
         else
             render 'new'
         end
